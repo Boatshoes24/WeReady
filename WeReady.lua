f25ca4393482678445d4d9e6677af2d455425775
Boatshoes24 <wmoulton4@gmail.com>
2017-09-13 20:45:50 -0400

    added new group check option

--- WeReady.lua
+++ WeReady.lua
@@ -1,19 +1,24 @@
 SLASH_WEREADY_SLASHCMD1 = '/rc'
 SLASH_WEREADY_SLASHCMD2 = '/rdy'
 SLASH_WEREADY_SLASHCMD3 = '/readycheck'
+SLASH_WEREADY_SLASHCMD4 = '/wr'
 
 
 
 SlashCmdList['WEREADY_SLASHCMD'] = function(msg)
 	
-	leader = UnitIsGroupLeader("player")
-	assist = UnitIsGroupAssistant("player")
+	local leader = UnitIsGroupLeader("player")
+	local assist = UnitIsGroupAssistant("player")
+	local inGroup = isGroup()
 		
-	if (leader or assist) and (msg == SLASH_WEREADY_SLASHCMD1 or SLASH_WEREADY_SLASHCMD2 or SLASH_WEREADY_SLASHCMD3) then 
-		DoReadyCheck()
+	if inGroup then
+		if (leader or assist) and (msg == SLASH_WEREADY_SLASHCMD1 or SLASH_WEREADY_SLASHCMD2 or SLASH_WEREADY_SLASHCMD3 or SLASH_WEREADY_SLASHCMD4) then 
+			DoReadyCheck()
 	
-	else 
-		print("You do not have lead/assist!")
+		else
+			print("You do not have lead/assist!")
+	else
+		print("You are not in a group!")
 	end
 	
 end

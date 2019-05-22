1aa19a4e32923e99ec2d82cc29a14f6fd5a3a51a
Boatshoes24 <wmoulton4@gmail.com>
2019-05-21 23:02:31 -0400

    reverting change to if statement

--- WeReady.lua
+++ WeReady.lua
@@ -11,7 +11,7 @@ SlashCmdList['WEREADY_SLASHCMD'] = function(msg)
 	local inGroup = IsInGroup()
 		
 	if inGroup then
-		if (leader or assist) and (msg == SLASH_WEREADY_SLASHCMD1 or msg == SLASH_WEREADY_SLASHCMD2 or msg == SLASH_WEREADY_SLASHCMD3) then 
+		if (leader or assist) and (msg == SLASH_WEREADY_SLASHCMD1 or SLASH_WEREADY_SLASHCMD2 or SLASH_WEREADY_SLASHCMD3) then 
 			DoReadyCheck()	
 		else
 			print("You do not have lead or assist!")			
